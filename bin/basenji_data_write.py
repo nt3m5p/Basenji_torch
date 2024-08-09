ed4036b5df77bb76c5d994b49bc85f62c0052e33
LeZ <le.zhang945@gmail.com>
2024-08-09 16:06:25 +0800

    添加了setup文件

--- bin/basenji_data_write.py
+++ bin/basenji_data_write.py
@@ -1,35 +1,16 @@
 #!/usr/bin/env python
-# Copyright 2019 Calico LLC
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     https://www.apache.org/licenses/LICENSE-2.0
 
 import os
 import sys
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# =========================================================================
 from optparse import OptionParser
-
 import h5py
 import numpy as np
 import pysam
 import torch
-
 from basenji.dna_io import dna_1hot
 from bin.basenji_data import ModelSeq
 
-"""
-basenji_data_write.py
-
-Write TF Records for batches of model sequences.
-"""
 
 
 ################################################################################
