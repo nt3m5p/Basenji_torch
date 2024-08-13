dc68ec4789d290ed1fda8587b1a1d4508eafa8b1
LeZ <le.zhang945@gmail.com>
2024-08-13 13:10:08 +0800

    修复了bug

--- basenji/trainer.py
+++ basenji/trainer.py
@@ -1,5 +1,6 @@
 import time
 
+import numpy as np
 import torch
 from torch import nn
 from torchmetrics.regression import PearsonCorrCoef
@@ -34,6 +35,33 @@ class Trainer:
                 r[i, j] = PearsonCorrCoef()(a[i, :, j], b[i, :, j])
         return r
     
+    def pearson_r(self,y_pred, y_true):
+        true_mean = torch.mean(y_true, dim=1)
+        true_mean2 = torch.square(true_mean)
+        pred_mean = torch.mean(y_pred, dim=1)
+        pred_mean2 = torch.square(pred_mean)
+        term1 = torch.sum(torch.multiply(y_true, y_pred),1)
+        
+        true_sum = torch.sum(y_true, dim=1)
+        pred_sum = torch.sum(y_pred, dim=1)
+        term2 = -torch.multiply(true_mean, pred_sum)
+        term3 = -torch.multiply(pred_mean, true_sum)
+        
+        count = torch.ones_like(y_true)
+        count = torch.sum(count, dim=1)
+        term4 = torch.multiply(count, torch.multiply(true_mean, pred_mean))
+        
+        covariance = term1 + term2 + term3 + term4
+        
+        true_sumsq = torch.sum(torch.square(y_true), dim=1)
+        pred_sumsq = torch.sum(torch.square(y_pred), dim=1)
+        true_var = true_sumsq - torch.multiply(count, true_mean2)
+        pred_var = pred_sumsq - torch.multiply(count, pred_mean2)
+        pred_var = torch.where(torch.greater(pred_var, 1e-12), pred_var, np.inf*torch.ones_like(pred_var))
+        tp_var = torch.multiply(torch.sqrt(true_var), torch.sqrt(pred_var))
+        correlation = torch.divide(covariance, tp_var)
+        return correlation
+    
     def train(self, epoch):
         self.model.train()
         start_time = time.time()
@@ -48,7 +76,9 @@ class Trainer:
             loss = output - target * torch.log(output)
             loss = torch.mean(loss, dim=(1, 2))# + self.model.l2_regularization(0.001).to('cpu')
             loss_list.extend(loss.tolist())
-            pearsonr_list.extend(self.pearson_correlation_coefficient(output, target).mean(dim=1).tolist())
+            t = self.pearson_r(target, output).mean(dim=1).tolist()
+            t2 = self.pearson_correlation_coefficient(output, target).mean(dim=1).tolist()
+            pearsonr_list.extend(self.pearson_r(output, target).mean(dim=1).tolist())
             loss.mean().backward()
             nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=self.clip_norm)
             self.optimizer.step()
@@ -85,7 +115,7 @@ class Trainer:
                     loss = output - target * torch.log(output)
                     loss = torch.mean(loss, dim=(1, 2))
                     t_loss_list.extend(loss.tolist())
-                    t_pearsonr_list.extend(self.pearson_correlation_coefficient(output, target).mean(dim=1).tolist())
+                    t_pearsonr_list.extend(self.pearson_r(output, target).mean(dim=1).tolist())
             
             t_loss = sum(t_loss_list) / len(t_loss_list)
             t_pearsonr = sum(t_pearsonr_list) / len(t_pearsonr_list)
